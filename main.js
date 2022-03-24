b7689087ffe485aea69d9cda2fd449f4d616f8b1
kerby-salvador <102286766+kerby-salvador@users.noreply.github.com>
2022-03-24 19:12:31 +0800

    further improved lib files

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-const newAccount = new PatientAccount({
-    username: "MCheddarson",
-    password: "CheeseHentai2531",
-    emailAddress: "mcheddarson@gmail.com",
-});
+const database = new Database();
 
-console.log(newAccount);
+database.patients.push(new PatientAccount({
+    name: {
+        given: "Mozzarella",
+        middle: "Lactose",
+        sur: "Chedddarson",
+    },
+    username: "MCheddarson",
+    password: "Ch33seH3nt4!",
+    emailAddress: "mcheddarson@cheese.com",
+}));
+
+console.log(database.searchPatientByID("1").name.givenFirst());
